83e701cfcab69cb16e2466454e497d5e99c99b84
chaitanyashirodkar010 <chaitanyashirodkar010@gmail.com>
2023-07-10 19:29:44 +0530

    jj

--- src/app/page-layout/side-navbar/side-navbar.component.ts
+++ src/app/page-layout/side-navbar/side-navbar.component.ts
@@ -1,40 +1,8 @@
 import { FlatTreeControl } from '@angular/cdk/tree';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
 // import { IUser } from 'src/app/interface/userInterface';
 import { UserInfoService } from 'src/app/service/user-info.service';
-
-interface FoodNode {
-  name: string;
-  children?: FoodNode[];
-}
-
-const TREE_DATA: FoodNode[] = [
-  {
-    name: 'Fruit',
-    children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
-  },
-  {
-    name: 'Vegetables',
-    children: [
-      {
-        name: 'Green',
-        children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
-      },
-      {
-        name: 'Orange',
-        children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
-      },
-    ],
-  },
-];
-
-interface ExampleFlatNode {
-  expandable: boolean;
-  name: string;
-  level: number;
-}
-
 
 @Component({
   selector: 'app-side-navbar',
@@ -42,36 +10,15 @@ interface ExampleFlatNode {
   styleUrls: ['./side-navbar.component.scss']
 })
 export class SideNavbarComponent {
-
-  private _transformer = (node: FoodNode, level: number) => {
-    return {
-      expandable: !!node.children && node.children.length > 0,
-      name: node.name,
-      level: level,
-    };
-  };
-
-  treeControl = new FlatTreeControl<ExampleFlatNode>(
-    node => node.level,
-    node => node.expandable,
-  );
-
-  treeFlattener = new MatTreeFlattener(
-    this._transformer,
-    node => node.level,
-    node => node.expandable,
-    node => node.children,
-  );
-
-  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
-  // user: IUser;
-
   constructor(private userInforService: UserInfoService) {
-    this.dataSource.data = TREE_DATA;
-   }
+
+  }
 
   ngOnInit() {
-    // this.user = this.userInforService.getLoggedInUser();
+
   }
-  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+  toggleDropdown(){
+    
+  }
+
 }
